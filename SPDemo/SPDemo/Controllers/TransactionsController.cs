552ad43108f5f3d52adae001fd28e65ccbee6e12
robby <arsyadanirobby36@gmail.com>
2018-12-09 21:22:32 +0700

    crud SP TransactionDB

---
+++ SPDemo/SPDemo/Controllers/TransactionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SPDemo.Models;
+
+namespace SPDemo.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        TransactionDB transacDB = new TransactionDB();
+        ItemDB itemDB = new ItemDB();
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public JsonResult Add(TransactionDB transac)
+        {
+            return Json(transacDB.Add(transac), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetItem()
+        {
+            var Item = itemDB.ListAll().ToList();
+            return Json(Item, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
