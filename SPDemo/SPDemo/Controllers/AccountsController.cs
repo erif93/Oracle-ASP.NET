37072aea4bed496fa34dc240c5cc124a3bccc8a3
Andri Ronaldo <andrironaldo43@gmail.com>
2018-12-11 08:23:28 +0700

    CRUD Account (View, Controller, Model, js, Encode/Decode Function)

---
+++ SPDemo/SPDemo/Controllers/AccountsController.cs
@@ -0,0 +1,63 @@
+﻿using SPDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPDemo.Controllers
+{
+    public class AccountsController : Controller
+    {
+        AccountDB AccountDB = new AccountDB();
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult List()
+        {
+            return Json(AccountDB.ListAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Accounts/Details/5
+        public JsonResult GetbyID(int ID)
+        {
+            var account = AccountDB.ListAll().Find(x => x.ID.Equals(ID));
+            account.PASSWORD = DecryptString(account.PASSWORD);
+            return Json(account, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Accounts/Create
+        public JsonResult Add(AccountDB account)
+        {
+            return Json(AccountDB.Add(account), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Update(AccountDB account)
+        {
+            return Json(AccountDB.Update(account), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Delete(int ID)
+        {
+            return Json(AccountDB.Delete(ID), JsonRequestBehavior.AllowGet);
+        }
+        public string DecryptString(string encrString)
+        {
+            byte[] b;
+            string decrypted;
+            try
+            {
+                b = Convert.FromBase64String(encrString);
+                decrypted = System.Text.ASCIIEncoding.ASCII.GetString(b);
+            }
+            catch (FormatException fe)
+            {
+                decrypted = "";
+            }
+            return decrypted;
+        }
+    }
+}
--- SPDemo/SPDemo/Models/AccountDB.cs
+++ SPDemo/SPDemo/Models/AccountDB.cs
@@ -1,86 +1,80 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Web;
-using Oracle.ManagedDataAccess.Client;
 
 namespace SPDemo.Models
 {
-    public class ItemDB
+    public class AccountDB
     {
         public int ID { get; set; }
-        public string NAME { get; set; }
-        public decimal PRICE { get; set; }
-        public decimal STOCK { get; set; }
+        public string USERNAME { get; set; }
+        public string PASSWORD { get; set; }
 
-        public ItemDB() { }
+        public AccountDB() { }
         //create Conection string
         string cs = ConfigurationManager.ConnectionStrings["OracleDBContext"].ConnectionString;
 
         //return list of all item
-        public List<ItemDB> ListAll()
+        public List<AccountDB> ListAll()
         {
-            List<ItemDB> lst = new List<ItemDB>();
+            List<AccountDB> lst = new List<AccountDB>();
             OracleConnection con = new OracleConnection();
             con.ConnectionString = cs;
             con.Open();
-            string query = "select * from transaction.item";
+            string query = "select * from transaction.account";
             OracleCommand com = new OracleCommand(query, con);
             //com.CommandType = CommandType.StoredProcedure;
             OracleDataReader rdr = com.ExecuteReader();
             while (rdr.Read())
             {
-                lst.Add(new ItemDB
+                lst.Add(new AccountDB
                 {
                     ID = Convert.ToInt32(rdr["ID"]),
-                    NAME = rdr["NAME"].ToString(),
-                    PRICE = Convert.ToInt32(rdr["PRICE"]),
-                    STOCK = Convert.ToInt32(rdr["STOCK"]),
+                    USERNAME = rdr["USERNAME"].ToString(),
+                    PASSWORD = rdr["PASSWORD"].ToString(),
                 });
             }
             return lst;
         }
 
         //Method for Adding an ItemDB  
-        public int Add(ItemDB itemDB)
+        public int Add(AccountDB accountDB)
         {
             int i;
             using (OracleConnection con = new OracleConnection(cs))
             {
                 con.Open();
-                string query = "Insert into TRANSACTION.ITEM(Name,Price,Stock) VALUES (:2, :3, :4)";
+                string query = "Insert into TRANSACTION.Account(Username,Password) VALUES (:2, :3)";
                 OracleCommand com = con.CreateCommand();
                 com.CommandText = query;
                 com.CommandType = CommandType.Text;
                 //com.CommandType = CommandType.StoredProcedure;
-                com.Parameters.Add("2", OracleDbType.Varchar2, 50).Value = itemDB.NAME.ToString();
-                com.Parameters.Add("3", OracleDbType.Double, 10).Value = Convert.ToDouble(itemDB.PRICE);
-                com.Parameters.Add("4", OracleDbType.Int16, 10).Value = Convert.ToInt16(itemDB.STOCK);
+                com.Parameters.Add("2", OracleDbType.Varchar2, 50).Value = accountDB.USERNAME.ToString();
+                com.Parameters.Add("3", OracleDbType.Varchar2, 50).Value = EnryptString(accountDB.PASSWORD.ToString());
                 i = com.ExecuteNonQuery();
             }
             return i;
         }
 
         //Method for Updating ItemDB record  
-        public int Update(ItemDB itemDB)
+        public int Update(AccountDB accountDB)
         {
             int i;
             using (OracleConnection con = new OracleConnection(cs))
             {
                 con.Open();
-                string query = "Update TRANSACTION.ITEM SET name=:2, price=:3, stock=:4 WHERE id=:1";
+                string query = "Update TRANSACTION.ACCOUNT SET username=:2, password=:3 WHERE id=:1";
                 OracleCommand com = con.CreateCommand();
                 com.CommandText = query;
                 com.CommandType = CommandType.Text;
                 //com.CommandType = CommandType.StoredProcedure;
-                com.Parameters.Add("2", OracleDbType.Varchar2, 50).Value = itemDB.NAME.ToString();
-                com.Parameters.Add("3", OracleDbType.Int32, 10).Value = Convert.ToInt32(itemDB.PRICE);
-                com.Parameters.Add("4", OracleDbType.Int32, 10).Value = Convert.ToInt32(itemDB.STOCK);
-
-
-                com.Parameters.Add("1", OracleDbType.Int32, 10).Value = Convert.ToInt32(itemDB.ID);
+                com.Parameters.Add("2", OracleDbType.Varchar2, 50).Value = accountDB.USERNAME.ToString();
+                com.Parameters.Add("3", OracleDbType.Varchar2, 50).Value = EnryptString(accountDB.PASSWORD.ToString());
+                com.Parameters.Add("1", OracleDbType.Int32, 10).Value = Convert.ToInt32(accountDB.ID);
                 i = com.ExecuteNonQuery();
             }
             return i;
@@ -93,7 +87,7 @@ namespace SPDemo.Models
             using (OracleConnection con = new OracleConnection(cs))
             {
                 con.Open();
-                string query = "Delete from TRANSACTION.ITEM WHERE id=:1";
+                string query = "Delete from TRANSACTION.ACCOUNT WHERE id=:1";
                 OracleCommand com = con.CreateCommand();
                 com.CommandText = query;
                 com.CommandType = CommandType.Text;
@@ -102,6 +96,13 @@ namespace SPDemo.Models
                 i = com.ExecuteNonQuery();
             }
             return i;
+        }        
+
+        public string EnryptString(string strEncrypted)
+        {
+            byte[] b = System.Text.ASCIIEncoding.ASCII.GetBytes(strEncrypted);
+            string encrypted = Convert.ToBase64String(b);
+            return encrypted;
         }
     }
 }
